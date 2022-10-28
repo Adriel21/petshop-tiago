da09e0e5197618f0c3d0d5821c75cac885094b34
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-10-28 09:59:23 -0300

    Inserção de eventos e hook useState
    Aolicação na página de contato

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,7 +1,19 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 import Caixa from "../../components/Caixa/Caixa";
 import estilos from "./Contato.module.css";
 const Contato = () => {
+  // Eventos/funções  para captura da digitação dos campos
+  const inputNome = (event) => setNome(event.target.value);
+  const inputEmail = (event) => setEmail(event.target.value);
+  const inputMensagem = (event) => setMensagem(event.target.value) remove.;
+1
+  // Hook useState para manipu
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale Conosco</h2>
@@ -10,12 +22,15 @@ const Contato = () => {
         {/* Não é necessário inserir o action pois é autogerenciado pelo react */}
           <form action="" method="post" className={estilos.formulario}>
             <div>
-              <TextField  required type="text" label="Nome Completo" variant="outlined" size="medium" fullWidth helperText="Você deve digitar um nome"/>
+              {/* Propriedades para eventos sempre começam com On */}
+              {/* Sempre coloque a função sem parêntese para que ela só seja executada quando for chamada */}
+              <TextField  onChange={inputNome} required type="text" label="Nome Completo" variant="outlined" size="medium" fullWidth helperText="Você deve digitar um nome"/>
 
               </div>
 
               <div>
                 <TextField 
+                onChange={inputEmail}
                   type="email"
                   label="E-mail"
                   variant="outlined"
@@ -27,7 +42,7 @@ const Contato = () => {
               </div>
 
               <div>
-                <TextField type="text" required label="Mensagem" variant="outlined" helperText="Fale o que você quiser" fullWidth multiline rows="6" />
+                <TextField onChange={inputMensagem} type="text" required label="Mensagem" variant="outlined" helperText="Fale o que você quiser" fullWidth multiline rows="6" />
               </div>
 
               <div>
