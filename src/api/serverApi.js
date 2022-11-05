78f882d8d22f2560d86131f67c66d09c2b6cd66d
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-11-05 08:18:04 -0300

    Configurando PWA

--- src/api/serverApi.js
+++ src/api/serverApi.js
@@ -1,5 +1,7 @@
 const porta = "2112";
-const serverApi = `http://localhost:${porta}`;
+// const serverApi = `http://localhost:${porta}`;
+const serverApi = `http://192.168.18.60:${porta}`;
+
 
 export default serverApi;
  
